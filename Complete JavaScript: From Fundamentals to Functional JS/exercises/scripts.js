a0b4aa482407579969c44471115729998c7b9c90
thefriendlydev <matt@milleronrails.com>
2015-11-09 15:59:18 -0500

    arrays + exercises

--- Complete JavaScript: From Fundamentals to Functional JS/exercises/scripts.js
+++ Complete JavaScript: From Fundamentals to Functional JS/exercises/scripts.js
@@ -22,3 +22,30 @@ for (var key in animal) {
 
 /* What happens if you return 'Hi my name is ' + ___ instead of using console.log() inside the loop? */
 // You wouldn't print the 'I like to say ' statement because you've returned from the loop
+
+
+/* https://github.com/bgando/array-exercises/ */
+var noiseArray = ['ribbit']
+noiseArray.unshift('croak')
+noiseArray.push('caw')
+noiseArray[3] = 'moo'
+
+console.log(noiseArray.length)
+console.log(noiseArray[noiseArray.length - 1])
+
+animal.noises = noiseArray // nested data structure
+
+// What are the different ways you can add properties and values to arrays?
+// You can add items using bracket notation or via the methods special to an array like push and unshift
+
+// What are the different ways you can add properties and values to arrays?
+//  You could use array[array.length] = 'new value' or array.push('new value')
+
+var animals = []
+animals.push(animal)
+
+var quackers = { username: 'DaffyDuck', tagline: 'Yippeee!', noises: ['quack', 'honk', 'sneeze', 'growl'] }
+animals[animals.length] = quackers
+
+animals.push({ username: 'Sassy', tagline: 'Sweetpea is sooooo lame', noises: ['squeek', 'huff', 'sneeze', 'growl'] })
+animals.push({ username: 'Sweetpea', tagline: 'Whaaaaattttt?', noises: ['ruff', 'rawr', 'click', 'zing'] })
