84b7291c5b761f28995a9703cfcfddd600373de5
thefriendlydev <matt@milleronrails.com>
2015-11-09 17:14:12 -0500

    function lesson

---
+++ Complete JavaScript: From Fundamentals to Functional JS/03_functions.js
@@ -0,0 +1,26 @@
+// A function is an object that does something, can assign them as variables and pass them around
+// arguments returns all of the arguments in an array like object, you can't use some array functions
+
+// function with capital means it's a constructor
+
+function AnimalMaker (name) {
+  return {
+    speak: function () {
+      console.log('my name is', name)
+    },
+    name: name,
+    owner: 'Matt'
+  } // object literal
+}
+
+var animalNames = ['Chameleon', 'Frog', 'Dog']
+
+var farm = []
+
+for (var i = 0; i < animalNames.length; i++) {
+  farm.push(AnimalMaker(animalNames[i]))
+}
+
+for (var i = 0; i < farm.length; i++) {
+  farm[i].speak()
+}
